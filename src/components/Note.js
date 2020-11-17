fe0eb81bfdd482ff91523748aacb18c9e3acf723
Michael Hu <30901394+mhucanada@users.noreply.github.com>
2020-11-17 12:44:27 -0500

    Update Note.js

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
+//react arrow function component
 const Note = ({ note, toggleImportance }) => {
   const label = note.important
     ? 'make not important' : 'make important'
